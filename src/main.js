b884a71492524f01388534ead2917a30ad4ba519
Dani Sarabia <dlsm@gft.com>
2022-07-05 21:03:10 +0200

    added files

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import BoardView from './components/BoardView.vue'
-import './assets/scss/style.scss'
-import './assets/scss/main.scss'
-createApp(BoardView).mount('#app')
-
